d853b48612feade9eaf137fb6f1526ffb24264ef
Kumar Appaiah <akumar@debian.org>
2009-07-26 00:09:05 -0400

    Add the common.h
    common.h added. It should have been added long ago.
    
    Signed-off-by: Kumar Appaiah <akumar@debian.org>

---
+++ common.h
@@ -0,0 +1,39 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <magic.h>
+#include <pcre.h>
+
+struct media_file_tags {
+  char *title;
+  char *artist;
+  char *album;
+  char *year;
+  char *genre;
+  char *comment;
+  char *track;
+  char *composer;
+  char *orig_artist;
+  char *copyright;
+  char *url;
+  char *encoded_by;
+};
+
+struct tag_regexes {
+  pcre *title_regex;
+  pcre *artist_regex;
+  pcre *album_regex;
+  pcre *year_regex;
+  pcre *genre_regex;
+  pcre *comment_regex;
+  pcre *track_regex;
+  pcre *composer_regex;
+  pcre *orig_artist_regex;
+  pcre *copyright_regex;
+  pcre *url_regex;
+  pcre *encoded_by_regex;
+  magic_t magic_handle;
+};
+
+
+#endif
